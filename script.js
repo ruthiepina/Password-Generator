d4a30f7c154fb89c26aefc27e568a6401cea61d7
RMP <ruthiepina@gmail.com>
2023-11-05 17:24:30 -0700

    created criteria arrays, consolidated variables in a pwdData array of objects

--- script.js
+++ script.js
@@ -3,13 +3,113 @@ const Criteria_Number = 4;
 const Password_Min_Length = 8;
 const Password_Max_Length = 128;
 
-var criteria2 = [
+var pwdData = [
    {
-      name: "lowercase",
-      selected: false,
-      totalChars: 0,
-      assignedChars: 0,
-      setOfChars: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"],
+      name: "uppercase characters",
+      selectedCriteria: false,
+      characterSet: [
+         "A",
+         "B",
+         "C",
+         "D",
+         "E",
+         "F",
+         "G",
+         "H",
+         "I",
+         "J",
+         "K",
+         "L",
+         "M",
+         "N",
+         "O",
+         "P",
+         "Q",
+         "R",
+         "S",
+         "T",
+         "U",
+         "V",
+         "W",
+         "X",
+         "Y",
+         "Z",
+      ],
+   },
+   {
+      name: "lowercase characters",
+      selectedCriteria: false,
+      characterSet: [
+         "a",
+         "b",
+         "c",
+         "d",
+         "e",
+         "f",
+         "g",
+         "h",
+         "i",
+         "j",
+         "k",
+         "l",
+         "m",
+         "n",
+         "o",
+         "p",
+         "q",
+         "r",
+         "s",
+         "t",
+         "u",
+         "v",
+         "w",
+         "x",
+         "y",
+         "z",
+      ],
+   },
+   {
+      name: "special characters",
+      selectedCriteria: false,
+      characterSet: [
+         "!",
+         '"',
+         "#",
+         "$",
+         "%",
+         "&",
+         "'",
+         "(",
+         ")",
+         "*",
+         "+",
+         ",",
+         "-",
+         ".",
+         "/",
+         ":",
+         ";",
+         "<",
+         "=",
+         ">",
+         "?",
+         "@",
+         "[",
+         "\\",
+         "]",
+         "^",
+         "_",
+         "`",
+         "{",
+         "|",
+         "}",
+         "~",
+      ],
+   },
+   {
+      name: "numeric characters",
+      selectedCriteria: false,
+      characterSet: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
    },
 ];
 
@@ -24,15 +124,26 @@ function getLengthCriteria(pwdLength) {
    return pwdLength;
 }
 
-function getCharTypes(CriteriaSelection) {}
+function getCharTypes() {
+   for (var i = 0; i < pwdData.length; i++) {
+      pwdData[i].selectedCriteria = confirm("Do you want to use " + pwdData[i].name + " to create a password?");
+   }
+}
+
+//* Initialize selected criteria array.
+for (var i = 0; i < pwdData.length; i++) {
+   pwdData[i].selectedCriteria = false;
+}
+
+var pwdLength = 0;
 
 function askCriteria() {
    //TODO: asl for password length
-   var pwdLength = 0;
-   pwdLength = getLengthCriteria(pwdLength);
+   pwdLength = getLengthCriteria();
+   console.log("My value: ", pwdLength);
    //TODO: ask for char type
-   var CriteriaSelection = { uppercase: false, lowercase: false, numeric: false, specialChar: false };
    getCharTypes();
+   console.log(pwdData);
    //TODO: validation of criteria selection
    //TODO: generate password
    //TODO: display password
